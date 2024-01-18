1818490d6fd3f51bc07095ffa0b3b8b683a8fd6b
Erik Taubeneck <erik.taubeneck@gmail.com>
2024-01-18 15:18:25 -0800

    update footer

--- server/app/footer.tsx
+++ server/app/footer.tsx
@@ -3,7 +3,7 @@ export default function Example() {
     <footer className="bg-white dark:bg-slate-950 h-full sticky top-[100vh]">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-4">
         <p className="text-center text-xs leading-5 text-gray-500">
-          &copy; 2023 Erik Taubeneck. All rights reserved.{" "}
+          &copy; 2023. All rights reserved.{" "}
           <a
             href="https://github.com/eriktaubeneck/draft"
             className="text-gray-800 dark:text-gray-200 hover:text-gray-600 hover:dark:text-gray-400"
