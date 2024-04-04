aed46c46614f75428d8b0bb534736648675c01b4
Erik Taubeneck <erik.taubeneck@gmail.com>
2024-04-03 22:53:20 -0700

    Form race condition (#12)
    * remove unneccesary logs
    
    * prevent form submission until commit hash populates via github api

--- server/app/query/create/page.tsx
+++ server/app/query/create/page.tsx
@@ -31,7 +31,6 @@ export default function Page() {
       const newQueryId = NewQueryId();
       setQueryId(newQueryId);
       // Send a POST request to start the process
-      console.log("sending post");
       const formData = new FormData(event.currentTarget);
       for (const remoteServer of Object.values(remoteServers)) {
         const response = await fetch(remoteServer.startURL(newQueryId), {
@@ -39,11 +38,8 @@ export default function Page() {
           body: formData,
         });
         const data = await response.json();
-        console.log(remoteServer);
-        console.log(data);
       }
 
-      // const data = await response.json();
       await new Promise((f) => setTimeout(f, 1000));
 
       // Redirect to /query/view/<newQueryId>
@@ -327,9 +323,10 @@ function IPAForm({
         type="submit"
         className={clsx(
           "mt-4 inline-flex items-center rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-emerald-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600",
-          !validCommitHash && "opacity-25 hover:bg-emerald-600",
+          (!validCommitHash || selectedCommitHash === "") &&
+            "opacity-25 hover:bg-emerald-600",
         )}
-        disabled={!validCommitHash}
+        disabled={!validCommitHash || selectedCommitHash === ""}
       >
         Start Query
       </button>
