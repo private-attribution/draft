b95fc7889afe6aebcd460bf452aa1fe04f6be725
Erik Taubeneck <erik.taubeneck@gmail.com>
2024-05-20 14:50:23 -0700

    update deployment to also use helper party domain names

--- etc/start_helper_sidecar.sh
+++ etc/start_helper_sidecar.sh
@@ -10,9 +10,10 @@ fi
 config_path=$1
 root_path=$2
 root_domain=$3
-sidecar_domain=$4
-helper_port=$5
-sidecar_port=$6
-identity=$7
+helper_domain=$4
+sidecar_domain=$5
+helper_port=$6
+sidecar_port=$7
+identity=$8
 
-nohup draft run-helper-sidecar --config_path "$config_path" --root_path "$root_path" --root_domain "$root_domain" --sidecar_domain "$sidecar_domain" --helper_port "$helper_port" --sidecar_port "$sidecar_port" --identity "$identity" > .draft/logs/helper_sidecar.log 2>&1 & echo $! > $pid_file
+nohup draft run-helper-sidecar --config_path "$config_path" --root_path "$root_path" --root_domain "$root_domain" --helper_domain "$helper_domain" --sidecar_domain "$sidecar_domain" --helper_port "$helper_port" --sidecar_port "$sidecar_port" --identity "$identity" > .draft/logs/helper_sidecar.log 2>&1 & echo $! > $pid_file
