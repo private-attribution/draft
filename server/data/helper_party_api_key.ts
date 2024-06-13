12fa61ed36e757775c26d07845b2c8c9452e95fb
Erik Taubeneck <erik.taubeneck@gmail.com>
2024-06-13 16:40:17 -0700

    update POST route to support updated api_key structure

--- server/data/helper_party_api_key.ts
+++ server/data/helper_party_api_key.ts
@@ -11,13 +11,17 @@ export async function validateApiKey(
   const currentTimestamp = new Date().toISOString();
   const { data, error } = await supabase
     .from("helper_party_api_keys")
-    .select("hashed_api_key, revoked, expires_at")
+    .select("hashed_api_key, expires_at, modified_reason")
     .eq("helper_party_uuid", helperPartyUUID)
     .order("created_at", { ascending: false });
 
   if (error) {
     console.error("Error fetching API key from database:", error);
-    throw error;
+
+    return {
+      isValid: false,
+      error: Error(error.message),
+    };
   }
 
   if (!data) {
@@ -26,14 +30,18 @@ export async function validateApiKey(
   }
 
   for (let row of data) {
+    // We need to loop through keys, to see if the provided key
+    // matches the stored hashed_api_key.
+    // If we find a match, then it's either expired, revoked, or value.
+    // If we don't find a match for the whole loop, it's just an invalid key.
     let valid = await bcrypt.compare(key, row.hashed_api_key);
     if (valid) {
       switch (true) {
-        case row.revoked: {
-          return { isValid: false, error: Error("API key revoked.") };
-        }
         case row.expires_at < currentTimestamp: {
           return { isValid: false, error: Error("API key expired.") };
+        }
+        case row.modified_reason === "REVOKED": {
+          return { isValid: false, error: Error("API key revoked.") };
         }
         default: {
           return { isValid: true, error: undefined };
