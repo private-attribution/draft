091fc0f000d1fed6ef78b06c7b831bf1352cf081
Erik Taubeneck <erik.taubeneck@gmail.com>
2024-06-27 07:32:48 -0700

    fix copy paste error

--- server/app/query/view/[id]/page.tsx
+++ server/app/query/view/[id]/page.tsx
@@ -149,10 +149,7 @@ export default function QueryPage({ params }: { params: { id: string } }) {
               Params:
             </dt>
             <dd className="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-              <ul
-<ul className="divide-y divide-gray-200 rounded-md border border-gray-200">
-                className="divide-y divide-gray-200 rounded-md border border-gray-200"
-              >
+              <ul className="divide-y divide-gray-200 rounded-md border border-gray-200">
                 {Object.entries(
                   JSON.parse((query?.params as string) || "{}"),
                 ).map(([key, value]) => {
